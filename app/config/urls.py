5c2983abc1b9772b18b305d7cebf2a401d2db174
HyungtaeMoon <blessmht@gmail.com>
2018-07-21 15:58:25 +0900

    localhost:8000 페이지로 진입 시 바로 posts 템플릿으로 연결
    config/views.py 추가, posts/urls.py에 app_name 추가
    app_name을 추가해야 posts:post-list의 형식으로 redirect 가능

--- app/config/urls.py
+++ app/config/urls.py
@@ -18,9 +18,12 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from config import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('posts/', include('posts.urls'))
+    path('', views.index),
+    path('posts/', include('posts.urls')),
 ] + static(
     prefix='media/',
     document_root=settings.MEDIA_ROOT,
