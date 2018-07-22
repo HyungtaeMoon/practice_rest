6e74ee14cd0668de9e8fcc97706bd5926f006934
HyungtaeMoon <blessmht@gmail.com>
2018-07-22 15:18:44 +0900

    [FIX][ADD] signup(회원가입) view 구현, base.html 수정
    signup view 구현,
    base.html에서 현재 유저가 누구인지 확인하는 {{user.username}}을 추가함
    그러나 앞으로도 html을 지속적으로 수정해야 할 필요가 있음

--- app/members/views.py
+++ app/members/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import authenticate
 
 from django.contrib.auth import login, logout
+from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 
 
@@ -25,3 +26,41 @@ def login_view(request):
 def logout_view(request):
         logout(request)
         return redirect('posts:post-list')
+
+
+def signup(request):
+    # context에 dic형식으로 담아서 에러메시지를 출력
+    context = {
+        'errors': [],
+    }
+    # 회원가입 양식은 post 방식으로 받아서
+    # request.POST 내용을 각각의(username, email, password, password2에 담아냄)
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # context에 POST로 받아온 username, email을 담아냄
+        context['username'] = username
+        context['email'] = email
+
+        # 만약 username이 이미 존재한다면
+        if User.objects.filter(username=username).exists():
+            # context에 아래와 같은 에러를 context에 append함
+            context['errors'].append('이미 존재합니다')
+        if password != password2:
+            context['errors'].append('패스워드가 일치하지 않습니다')
+
+        # 만약에 errors가 없다면 user를 생성하여 로그인을 함
+        if not context['errors']:
+            user = User.objects.create_user(
+                username=username,
+                email=email,
+                password=password,
+            )
+            login(request, user)
+            # 여기까지 왔다면 메인 페이지인 post-list로 이동
+            return redirect('posts:post-list')
+    # 그게 아니라면 get 방식인데 회원가입 창을 보여줌
+    return render(request, 'members/signup.html', context)
