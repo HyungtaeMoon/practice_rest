a215876170fcd56324b6447a4255cb775ee71d9d
HyungtaeMoon <blessmht@gmail.com>
2019-06-18 15:12:56 +0900

    다른 목적으로 사용하기 위한 저장소 초기화

--- app/posts/models.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-from django.db import models
-
-
-class Post(models.Model):
-    auth = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    photo = models.ImageField(upload_to='post', blank=True)
-    content = models.TextField(max_length=50)
-    created_at = models.DateTimeField(auto_now_add=True)
